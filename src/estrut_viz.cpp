62d1f64fcc3075f8baaac4bffcaa4a078d0b0d54
JulyannaAzevedo <julyanna.azevedo@academico.ufpb.br>
2023-10-09 22:26:44 -0300

    create estrut_viz

---
+++ src/estrut_viz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Recalcular a nova rota
+int recalcular(){
+    // Subtrair o valor do custo anterior e posterior do vertice atual
+    // Somar o valor do custo anterior e posterior do vertice novo
+    return;
+}
+
+// Realizar a troca de clientes em uma msm rota
+vector<int> swap(){
+    // Trocar cliente x por y (x!=y e x!=0 e y!=0)
+    return ;
+}
