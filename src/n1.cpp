012003dee10e3c92f218d96a0abb2380f9a4032c
JulyannaAzevedo <julyanna.azevedo@academico.ufpb.br>
2023-10-11 17:56:36 -0300

    Update n1

--- src/n1.cpp
+++ src/n1.cpp
@@ -13,41 +13,32 @@ struct Solution{
 
 void swapInside(int *total_cost,vector<vector<int>> c,vector<int> &v, int i, int j){
 
-    int ant_i = i-1;
-    int prox_i = i+1;
-    int ant_j = j-1;
-    int prox_j = j+1;
+    int ant_i = v[i-1];
+    int prox_i = v[i+1];
+    int ant_j = v[j-1];
+    int prox_j = v[j+1];
 
     if (i != j){
-        //remove o custo de quem estava ligado em i
-        *total_cost -= c[v[ant_i]][v[i]];
-        cout << "Apos remover a aresta [" << v[ant_i] << "][" << v[i] << "] o custo atual = " << *total_cost << endl;
-        *total_cost -= c[v[i]][v[prox_i]];
-        cout << "Apos remover a aresta [" << v[i] << "][" << v[prox_i] << "] o custo atual = " << *total_cost << endl;
 
+        //remove o custo de quem estava ligado em i
+        *total_cost -= c[ant_i][v[i]];
+        *total_cost -= c[v[i]][prox_i];
         
         //remove o custo de quem estava ligado em j
-
-        *total_cost -= c[v[ant_j]][v[j]];
-        cout << "Apos remover a aresta [" << v[ant_j] << "][" << v[j] << "] o custo atual = " << *total_cost << endl;
-        *total_cost -= c[v[j]][v[prox_j]];
-        cout << "Apos remover a aresta [" << v[j] << "][" << v[prox_j] << "] o custo atual = " << *total_cost << endl;
-       
+        *total_cost -= c[ant_j][v[j]];
+        *total_cost -= c[v[j]][prox_j];
 
         //adiciona o novo custo em relação a troca de j
-        *total_cost += c[v[ant_i]][v[j]];
-        cout << "Apos adicionar a aresta [" << v[ant_i] << "][" << v[j] << "] o custo atual = " << *total_cost << endl;
-        *total_cost += c[v[j]][v[prox_i]];
-        cout << "Apos adicionar a aresta [" << v[j] << "][" << v[prox_i] << "] o custo atual = " << *total_cost << endl;
+        *total_cost += c[ant_i][v[j]];
+        *total_cost += c[v[j]][prox_i];
 
         //adiciona o novo custo em relação a troca de i
-        *total_cost += c[v[ant_j]][v[i]];
-        cout << "Apos adicionar a aresta [" << v[ant_j] << "][" << v[i] << "] o custo atual = " << *total_cost << endl;
-        *total_cost += c[v[i]][v[prox_j]];
-        cout << "Apos adicionar a aresta [" << v[i] << "][" << v[prox_j] << "] o custo atual = " << *total_cost << endl;
+        *total_cost += c[ant_j][v[i]];
+        *total_cost += c[v[i]][prox_j];
+
         //se a diferença de J e I == 1 então ele removeu 2 arestas iguais
         if (j - i == 1){
-            *total_cost += c[v[i]][v[prox_i]];
+            *total_cost += c[v[i]][prox_i];
         }
         cout << "Custo total = " << *total_cost << endl;
 
@@ -55,6 +46,7 @@ void swapInside(int *total_cost,vector<vector<int>> c,vector<int> &v, int i, int
         int aux = v[i];
         v[i] = v[j];
         v[j] = aux;
+        
         //cout << "Custo atualizado = " << *total_cost << endl;
     }
 }
