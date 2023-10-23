0385e8e27c9caa4967f5fc8a4008b4c36a7c2f2d
JulyannaAzevedo <julyanna.azevedo@academico.ufpb.br>
2023-10-23 12:20:19 -0300

    Update ILS

--- src/ILS.cpp
+++ src/ILS.cpp
@@ -1,29 +1,22 @@
 /*
 
-Função ILS(maxIteracoes):
-    MelhorSolucao <- GereUmaSolucaoInicial()  # Gere uma solução inicial aleatória
-    MelhorCusto <- CalculeCusto(MelhorSolucao)  # Calcule o custo da melhor solução inicial
+Função ILS():
 
-    Para iteracao de 1 até maxIteracoes:
-        SolucaoVizinhanca <- RealizeBuscaLocal(MelhorSolucao)  # Aplique busca local para melhorar a solução
-        PerturbeSolucao(SolucaoVizinhanca)  # Introduza perturbações na solução
+    MelhorSolucao  = //Solução inicial q vem do construtor
+    MelhorCusto = CalculeCusto(MelhorSolucao)  # Calcule o custo da melhor solução inicial
 
-        CustoVizinhanca <- CalculeCusto(SolucaoVizinhanca)
+    Para (criterio de parada): // Pode ser até 10 vezes sem achar uma melhor solução ou um tempo máximo
+        SolucaoVizinhanca = VND(MelhorSolucao) // Busca local
+        PerturbeSolucao(SolucaoVizinhanca)  # Fazer perturbações na solução
+
+        CustoVizinhanca = CalculeCusto(SolucaoVizinhanca)
 
         Se CustoVizinhanca < MelhorCusto:
-            MelhorSolucao <- SolucaoVizinhanca
-            MelhorCusto <- CustoVizinhanca
+            MelhorSolucao = SolucaoVizinhanca
+            MelhorCusto = CustoVizinhanca
 
     Retorne MelhorSolucao e MelhorCusto
 
-maxIteracoes é o número máximo de iterações a serem executadas.
-GereUmaSolucaoInicial() é uma função que gera uma solução inicial aleatória.
-CalculeCusto(Solucao) é uma função que calcula o custo da solução.
-RealizeBuscaLocal(Solucao) é uma função que aplica busca local para melhorar a solução.
-PerturbeSolucao(Solucao) é uma função que introduz perturbações na solução, por exemplo, realizando trocas aleatórias.
-
 */
 
-// Pertubação não se preocupa com os valores, só mexe na solução
-// Busca Local é o próprio vnd
-// Criterio de parada pode ser até 10 vezes sem achar uma melhor solução ou um tempo máximo
+
