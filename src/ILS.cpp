4e78d593d4b1edf0b38eac872811aaa85dbbe320
Ruy <ruy.morais@academico.ufpb.br>
2023-10-23 15:32:23 -0300

    Bug fix ILS

--- src/ILS.cpp
+++ src/ILS.cpp
@@ -12,15 +12,29 @@ using namespace std;
 Solution pertubacao(Solution solucao, vector<vector<int>> c){
 
     // Inicializa o gerador de números aleátorios com o relógio
-    //srand((unsigned) time(0));
+    srand((unsigned) time(0));
+    
+    int num_rotas = solucao.routes.size(); //Colocar na struct a quantidade de rotas
+    cout << "Numero de rotas: " << num_rotas << endl;
+    int custo_inicial = solucao.totalCost;
 
-    cout << "rote_size: " << solucao.route_size[0] << endl;
+    //Para cada rota
+    for (int k = 0; k < num_rotas; k++){
 
-    int random_i = 1 + (rand() % (solucao.route_size[0]-2));
-    int random_j = 1 + (rand() % (solucao.route_size[0]-2));
-    //swapInside(solucao.routes[0], random_i, random_j);
-    int novo_custo = custoSwap(solucao.totalCost, c, solucao.routes[0], random_i, random_j);
-    cout << "custo: " << solucao.totalCost << endl;
+
+        // Escolhe um cliente aleatório da rota
+        int random_i = rand() % (solucao.route_size[k]-2) + 1;
+        int random_j = rand() % (solucao.route_size[k]-2) + 1;
+
+        cout << "Indices randomicos: " << random_i << " " << random_j << endl;
+        //swapInside(solucao.routes[k], random_i, random_j);
+
+        int novo_custo = custoSwap(solucao.totalCost, c, solucao.routes[k], random_i, random_j);
+        cout << "Custo inicial: " << custo_inicial << endl;
+        cout << "Novo custo: " << novo_custo << endl;
+
+    }
+    
     return solucao;
 }
 
@@ -28,7 +42,7 @@ Solution ILS(Solution solucao_inicial, int Q, int L, vector<int> d, vector<int> 
 
     Solution S0  = solucao_inicial;
     Solution S = vnd(S0, Q, L, d, p, c); // Busca local
-    int cont=0;
+    int cont= 0;
 
     while(cont <= 10){ // Pode ser até 10 vezes sem achar uma melhor solução ou um tempo máximo
         
